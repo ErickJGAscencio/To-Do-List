39317aae08c4fa3447b3aacd9fa1caddf68b18e1
Erick Jesús Guzmán Ascencio <erickjesus00001@gmail.com>
2024-10-17 15:27:26 -0600

    Add Parentesis

--- todolist_api/deployment_settings.py
+++ todolist_api/deployment_settings.py
@@ -5,7 +5,7 @@ from .settings import BASE_DIR
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
-CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 DEBUG = False
 
