92290ddf0ca1a45c74cc2db22b9c9a1807de539a
Erick Jesús Guzmán Ascencio <erickjesus00001@gmail.com>
2024-10-17 15:22:50 -0600

    Add Parentesis

--- todolist_api/deployment_settings.py
+++ todolist_api/deployment_settings.py
@@ -3,7 +3,7 @@ import dj_database_url
 from .settings import *			
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
 
