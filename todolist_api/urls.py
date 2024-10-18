573b03883fc508a42810b4a78f5cb6329311eec4
Erick Jesús Guzmán Ascencio <erickjesus00001@gmail.com>
2024-10-18 06:44:21 -0600

    Add route

--- todolist_api/urls.py
+++ todolist_api/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todolist/', include('todolist.urls')),
+    path('/', include('todolist.urls')),
 ]
