fa38f9bf33d675ed2b0f8bad32acc46c80176948
Erick Jesús Guzmán Ascencio <erickjesus00001@gmail.com>
2024-10-18 07:59:23 -0600

    Change route login

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework.documentation import include_docs_urls
 from rest_framework.routers import DefaultRouter
 from todolist import views
@@ -9,13 +10,13 @@ router.register(r'tasks', views.TaskViewSet)
 router.register(r'subtasks', views.SubTaskViewSet)
 
 urlpatterns = [
-  path('', include(router.urls)),
-  path('api/v1/', include(router.urls)),
+    path('', RedirectView.as_view(url='/login/', permanent=False)),  # Redirige la ruta vacía a /login/
+    path('api/v1/', include(router.urls)),
   
-  path("login/", views.login, name='login'),
-  path("register/", views.register, name='register'),
-  path("profile/", views.profile, name='profile'),
-  path('me/', views.profile, name='user-profile'),
+    path("login/", views.login, name='login'),
+    path("register/", views.register, name='register'),
+    path("profile/", views.profile, name='profile'),
+    path('me/', views.profile, name='user-profile'),
 
-  path('docs/', include_docs_urls(title='WorkCloud Documentation')),
+    path('docs/', include_docs_urls(title='WorkCloud Documentation')),
 ]
