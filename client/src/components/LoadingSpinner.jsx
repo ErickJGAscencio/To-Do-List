e98181e9110e8ee3705395f5e42e4c479295ccb2
Erick Jesús Guzmán Ascencio <erickjesus00001@gmail.com>
2024-10-25 13:47:57 -0600

    Implement Loading Spinner

---
+++ client/src/components/LoadingSpinner.jsx
@@ -0,0 +1,10 @@
+import { FaSpinner } from 'react-icons/fa';
+import '../pages/LogInPage.css';
+
+export const LoadingSpinner = () => {
+  return (
+    <div className="loading-container">
+      <FaSpinner className="loader" style={{ color: "#0884c4" }} />
+    </div>
+  );
+};
